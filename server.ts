4b92d0ebad15b6fc71dd17231e98805b63485908
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-16 16:48:57 +0530

    feat: implement routing for admin and user with respective controllers

--- server.ts
+++ server.ts
@@ -2,7 +2,11 @@ import dotenv from "dotenv";
 import express from "express";
 const app = express();
 dotenv.config();
+import adminRoutes from "./src/controllers/admin-controller.js";
+import userRoutes from "./src/controllers/user-controller.js";
 
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/user", userRoutes);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Working - Good Health", success: true });
 });
