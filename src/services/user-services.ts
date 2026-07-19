ef541dcdb168b64e3dbccb62d3eb2029dd724628
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-20 01:35:07 +0530

    feat: add unique email validation for user and admin schemas, enhance user service with email existence check

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -1,9 +1,15 @@
 import { Signup } from "../interfaces/user.js";
-import userRepository from "../repositories/user-repository.js";
+import UserRepository from "../repositories/user-repository.js";
+import { AppError } from "../utils/app-error.js";
 
 class UserServices {
   async create(body: Signup) {
-    return userRepository.create(body);
+    const { email } = body;
+    const emailExist = await UserRepository.findByEmail(email);
+    if (emailExist) {
+      throw new AppError("Email Already Exist", 400);
+    }
+    return UserRepository.create(body);
   }
 }
 
