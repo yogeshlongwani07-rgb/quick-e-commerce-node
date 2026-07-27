cff1b48b0973d7f6352d1f8e34ed1601336fb974
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-27 19:05:53 +0530

    feat: enhance user creation to generate and set authentication tokens in cookies

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import UserServices from "../services/user-services.js";
+import generateToken from "../utils/generateToken.js";
+import { setAuthCookies } from "../utils/setAuthCookies.js";
 
 class UserController {
   async create(req: Request, res: Response): Promise<void> {
-    await UserServices.create(req.body);
+    const user = await UserServices.create(req.body);
+    const { accessToken, refreshToken } = await generateToken(user);
+    setAuthCookies(res, accessToken, refreshToken);
     res
       .status(200)
       .json({ message: "Account created Successfully", success: true });
