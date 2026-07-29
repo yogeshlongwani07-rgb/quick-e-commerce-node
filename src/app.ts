bdaadffb83f7fc00cabaaf7e14dc661f38bc5f9a
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-07-29 18:07:19 +0530

    feat: implement Google OAuth integration with user creation and token handling

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/admin.js";
 import userRoutes from "./routes/user.js";
 import errorHandler from "./middleware/errorHandler.js";
+import OAuthRoutes from "./routes/oauth.js";
 
 export default function createApp() {
   const app = express();
@@ -10,11 +11,8 @@ export default function createApp() {
   app.use(cookieParser());
   app.use("/api/v1/admin", adminRoutes);
   app.use("/api/v1/user", userRoutes);
+  app.use("/api/v1/o/auth", OAuthRoutes);
   app.use(errorHandler);
 
   return app;
-
-  //   app.listen(process.env.PORT, () => {
-  //     console.log(`Server Connected ${process.env.PORT}`);
-  //   });
 }
